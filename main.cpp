0284ebd4cda5f575af54cc4973ecce3ca1db92f7
Karla C. Ferreira <karlinharural@gmail.com>
2023-08-23 17:02:21 -0300

    nossoJeito.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ string binario(int n){
         numBinario += to_string(resto);
         n /= 2;
     }
-    
     return  numBinario;
 }
 
@@ -24,7 +23,6 @@ int binarioBits(int n){
         numBinario += to_string(resto);
         n /= 2;
     }
-    cout << numBinario << endl;
     return  numBinario.length()-1;
 }
 
@@ -36,7 +34,15 @@ string binarioOrdenado(string binario, int binarioBits){
         j++;
     }
     return ordenacao;
-    
+}
+
+int calcDecimal(int a, int b){
+	for(int i = 0; i < 7; i++){
+	    int c = a & b;
+		a ^= b;
+		b = c << 1;
+	}
+	return a;
 }
 
 int main(){
@@ -44,12 +50,13 @@ int main(){
     int n1, n2;
     cout << "digite um numero: ";
     cin >> n1;
-   // cout << "digite o outro numero: ";
-   // cin >> n2;
-   // cout << binario(n1) << endl;
-    //cout << binario(n2);
-    binarioBits(n1);
-    //cout <<binarioOrdenado(binario(n1),binarioBits(n1));
+    
+    cout << "digite o outro numero: ";
+    cin >> n2;
+    cout << "o primeiro numero em binario eh: " << binarioOrdenado(binario(n1),binarioBits(n1));
+    cout << "\no segundo numero em binario eh: " << binarioOrdenado(binario(n2),binarioBits(n2));
+    cout << "\nA soma dos decimais eh: " << calcDecimal(n1, n2);
+    cout << "\nA soma em binario eh: " << binarioOrdenado(binario(calcDecimal(n1, n2)), binarioBits(calcDecimal(n1, n2)));
     
 
     return 0;
